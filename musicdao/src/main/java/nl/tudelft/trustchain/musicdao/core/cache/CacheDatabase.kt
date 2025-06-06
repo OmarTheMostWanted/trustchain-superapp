8a48b1d22b798c794f51e18720cd7f3d6b69eee4
David Iliopoulos <davidiliopoulos@gmail.com>
2025-06-06 12:00:02 +0200

    Add the entity

--- musicdao/src/main/java/nl/tudelft/trustchain/musicdao/core/cache/CacheDatabase.kt
+++ musicdao/src/main/java/nl/tudelft/trustchain/musicdao/core/cache/CacheDatabase.kt
@@ -11,7 +11,7 @@ import nl.tudelft.trustchain.musicdao.core.cache.entities.MusicTagEntity
 import nl.tudelft.trustchain.musicdao.core.cache.parser.Converters
 
 @Database(
-    entities = [AlbumEntity::class, MusicLikeEntity::class],
+    entities = [AlbumEntity::class, MusicLikeEntity::class, MusicTagEntity::class],
     version = 8,
     exportSchema = false
 )
